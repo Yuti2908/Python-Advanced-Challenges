ef4c8915eb583d0629fc3e91b2ad44caf5864d1f
Yutika Prabhu <92089277+Yuti2908@users.noreply.github.com>
2023-10-18 21:13:57 +0530

    Add files via upload

---
+++ MadLib/madlib.py
@@ -0,0 +1,11 @@
+print("Super Hero edition")
+print("==========================================================================")
+adj = input("Thoughts on Marvel: ")
+verb1 = input("Why do you like it?: ")
+verb2 = input("Verb for you favourite Marvel superhero: ")
+famous_person = input("Name that Hero: ")
+
+madlib = f"Marvel is {adj}! It makes me so excited all the time because \
+I love {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
